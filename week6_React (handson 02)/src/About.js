fbb12178da68751ab7fa227936ebcc7d27a81dda
parmitapani <156915213+parmitapani@users.noreply.github.com>
2025-07-28 19:22:52 +0530

    Add files via upload

---
+++ week6_React (handson 02)/src/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                <h3>About Student Management Portal</h3>
+                <p>This portal helps manage student information efficiently.</p>
+            </div>
+        );
+    }
+}
---
+++ week6_React (handson 02)/src/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Contact Us</h3>
+                <p>Email: support@studentportal.com</p>
+            </div>
+        );
+    }
+}
